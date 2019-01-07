f89dbf16a369cbc050071fcdc161a97e69940f9b
Brian Piltin <blpiltin@gmail.com>
2019-01-06 23:16:09 -0600

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -168,108 +168,200 @@ describe('PATCH /todos/:id', () => {
   });
 });
 
+describe('GET /users/me', () => {
+
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', USERS[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toEqual(USERS[0]._id.toHexString());
+        expect(res.body.email).toBe(USERS[0].email);
+      }).end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      }).end(done);
+  });
+
+});
+
 describe('POST /users', () => {
-  it('should create a new user', (done) => {
+  
+  it('should create a user', (done) => {
     var email = 'who3@ever.com';
-    var password = '123abc';
+    var password = '123abc!';
 
     request(app)
       .post('/users')
       .send({email, password})
       .expect(200)
       .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
         expect(res.body.email).toBe(email);
       })
-      .end((err, res) => {
+      .end((err) => {
         if (err) {
           return done(err);
         }
 
-        User.find({email}).then((users) => {
-          expect(users.length).toBe(1);
-          expect(users[0].email).toBe(email);
-          done();
-        }).catch((err) => done(err));
-      });
-  });
-
-  it('should not create user with bad data', (done) => {
-    request(app)
-      .post('/users')
-      .send({})
-      .expect(400)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-
-        User.find().then((users) => {
-          expect(users.length).toBe(USERS.length);
-          done();
-        }).catch((err) => done(err));
-      });
-  });
-
-  it('should not create user with duplicate email', (done) => {
-    var email = 'who1@ever.com';
-    var password = '123abc';
-
-    request(app)
-      .post('/users')
-      .send({email, password})
-      .expect(400)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-
-        User.find().then((users) => {
-          expect(users.length).toBe(USERS.length);
-          done();
-        }).catch((err) => done(err));
-      });
-  });
-
-  it('should not create user with invalid email', (done) => {
-    var email = 'who1@ever';
-    var password = '123abc';
-
-    request(app)
-      .post('/users')
-      .send({email, password})
-      .expect(400)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        
-        User.find().then((users) => {
-          expect(users.length).toBe(USERS.length);
-          done();
-        }).catch((err) => done(err));
-      });
-  });
-
-  it('should hash the user\'s password', (done) => {
-    var email = 'who3@ever.com';
-    var password = '123abc';
-
-    request(app)
-      .post('/users')
-      .send({email, password})
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        
         User.findOne({email}).then((user) => {
           expect(user).toExist();
-          expect(user.password).toExist();
           expect(user.password).toNotBe(password);
+          done();
+        });
+      });
+  });
+
+  it('should return validation errors if request invalid', (done) => {
+    request(app)
+      .post('/users')
+      .send({
+        email: 'and',
+        password: '123'
+      })
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.find().then((users) => {
+          expect(users.length).toBe(USERS.length);
           done();
         }).catch((err) => done(err));
       });
   });
 
+  it('should not create user if email in use', (done) => {
+    request(app)
+      .post('/users')
+      .send({
+        email: USERS[0].email, 
+        password: '123abc!'})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.find().then((users) => {
+          expect(users.length).toBe(USERS.length);
+          done();
+        }).catch((err) => done(err));
+      });
+  });
 });
+
+// describe('POST /users', () => {
+//   it('should create a new user', (done) => {
+//     var email = 'who3@ever.com';
+//     var password = '123abc';
+
+//     request(app)
+//       .post('/users')
+//       .send({email, password})
+//       .expect(200)
+//       .expect((res) => {
+//         expect(res.body.email).toBe(email);
+//       })
+//       .end((err, res) => {
+//         if (err) {
+//           return done(err);
+//         }
+
+//         User.find({email}).then((users) => {
+//           expect(users.length).toBe(1);
+//           expect(users[0].email).toBe(email);
+//           done();
+//         }).catch((err) => done(err));
+//       });
+//   });
+
+//   it('should not create user with bad data', (done) => {
+//     request(app)
+//       .post('/users')
+//       .send({})
+//       .expect(400)
+//       .end((err, res) => {
+//         if (err) {
+//           return done(err);
+//         }
+
+//         User.find().then((users) => {
+//           expect(users.length).toBe(USERS.length);
+//           done();
+//         }).catch((err) => done(err));
+//       });
+//   });
+
+//   it('should not create user with duplicate email', (done) => {
+//     var email = 'who1@ever.com';
+//     var password = '123abc';
+
+//     request(app)
+//       .post('/users')
+//       .send({email, password})
+//       .expect(400)
+//       .end((err, res) => {
+//         if (err) {
+//           return done(err);
+//         }
+
+//         User.find().then((users) => {
+//           expect(users.length).toBe(USERS.length);
+//           done();
+//         }).catch((err) => done(err));
+//       });
+//   });
+
+//   it('should not create user with invalid email', (done) => {
+//     var email = 'who1@ever';
+//     var password = '123abc';
+
+//     request(app)
+//       .post('/users')
+//       .send({email, password})
+//       .expect(400)
+//       .end((err, res) => {
+//         if (err) {
+//           return done(err);
+//         }
+        
+//         User.find().then((users) => {
+//           expect(users.length).toBe(USERS.length);
+//           done();
+//         }).catch((err) => done(err));
+//       });
+//   });
+
+//   it('should hash the user\'s password', (done) => {
+//     var email = 'who3@ever.com';
+//     var password = '123abc';
+
+//     request(app)
+//       .post('/users')
+//       .send({email, password})
+//       .expect(200)
+//       .end((err, res) => {
+//         if (err) {
+//           return done(err);
+//         }
+        
+//         User.findOne({email}).then((user) => {
+//           expect(user).toExist();
+//           expect(user.password).toExist();
+//           expect(user.password).toNotBe(password);
+//           done();
+//         }).catch((err) => done(err));
+//       });
+//   });
+
+// });
